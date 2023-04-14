4f15602649ce342fd861c7089d4067524e8f281a
Solid <solid@vidya.dev>
2023-04-14 16:30:47 -0400

    packages: remove partytown because it is unused right now

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,6 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
-import partytown from "@astrojs/partytown";
-
-// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), partytown()]
+  integrations: [tailwind()]
 });
